73cd1ab2eabf2e12f27a29bf7d4cf375154c5842
Neo <echiang73@gmail.com>
2019-06-05 10:56:25 -0700

    Clean up file

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -1,38 +1,3 @@
-// var request = require("request");
-// var cheerio = require("cheerio");
-
-// var scrape = function (cb) {
-//     request("https://www.medicalnewstoday.com/categories/colorectal_cancer", function(err, res, body) {
-//         var $ = cheerio.load(body);
-
-//         var articles = [];
-
-//         $(".headline").each(function (i, element) {
-//             var title = $(this).parent().attr("title");
-//             var summary = $(this).find("em").text();
-//             var imageurl = $(this).parent().find("img").attr("data-src");
-//             var link = "https://www.medicalnewstoday.com" + $(this).parent().attr("href");
-
-//             if(title && summary && imageurl && link){
-//                 var result = {
-//                     title: title,
-//                     summary: summary,
-//                     imageurl: imageurl,
-//                     link: link
-//                 };
-//                 articles.push(result);
-//             return i < 9;
-//             }
-//         });
-//         // console.log(articles);
-//         cb(articles);
-//         // res.send("Scrape Complete");
-//     });
-// };
-
-// module.exports = scrape;
-
-
 var axios = require("axios");
 var cheerio = require("cheerio");
 
